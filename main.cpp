486e28f2bc608a237c4070a22f96d3cbd6869052
sisipun <sisipun@gmail.com>
2024-04-01 14:04:32 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "leveleditor.h"
+
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    LevelEditor w;
+    w.show();
+    return a.exec();
+}
